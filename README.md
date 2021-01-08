fa4f6efbfd6432c16108c0a03aa19724e01ed4eb
Serge Rey <sjsrey@gmail.com>
2021-01-08 16:50:19 +0000

    Readme

--- README.md
+++ README.md
@@ -1,10 +1,46 @@
-# MANITEX
-A package for building a manifest and bundle for a LaTeX paper that has been accepted for publication.
+===================================================================
+                  The manitex Package
+===================================================================
+
+Author:  Serge Rey  
+Version:  0.01 (2021/01/08)  
+License:  LPPL v1.3 or later (LaTeX code) and BSD 3-Clause (Python code)  
+Development:  https://github.com/sjsrey/manitex
+Requirements:  Python 3+
+
+The manitext package builds a manifest of files for a Latex Article that has
+been accepted for publication. 
+
+## Features
+
+- archive of manifest in a zip file
+- automatic indexing of figures and graphic files
 
 
-## Instructions
+## Using 
 
-## Options
+If the main latex document is called `main.tex`, we first need to generate a list of files needed to compile the paper. To do this, add the following at the top of the main latex document:
 
-- include final pdf
-- include latex styles
+```
+\RequirePackage{snapshot}
+```
+
+Compiling the paper with `pdflatex main` will produce a file called `main.dep`
+that contains information that can be used to build the manifest. The
+`main.dep` file is required for `manitex` to work and an exception will be
+raised if the file is missing.
+
+
+### Building the manifest
+
+```
+manitex main.tex
+```
+Will produce the zip file `main.zip` that contains all the figures and main
+tex files required to build the pdf version of the article, along with a
+`README.md` file that provides an index/explanation for the copy-editor.
+
+
+
+
+
---
+++ manitex/manitex.py
@@ -0,0 +1,98 @@
+"""Copy figures used by document."""
+import os
+import os.path
+import shutil
+import argparse
+
+
+description = 'Build manifest for publication in LaTex.'
+parser = argparse.ArgumentParser(description=description)
+parser.add_argument('main', help="main file name", type=str)
+parser.add_argument("--outputdir", help="output directory for manifest",
+                    default="manifest")
+parser.add_argument("--extensions", help="image file extensions",
+                    default=['pdf', 'pdf_tex', 'png'])
+
+args = parser.parse_args()
+
+MAIN_FILE = args.main
+TARGET_DIR = args.outputdir
+EXTENSIONS = args.extensions
+
+
+# check if dep file has been built
+try:
+    DEP_FILE_BASE = os.path.splitext(MAIN_FILE)[0]
+    DEP_FILE = DEP_FILE_BASE + ".dep"
+    with open(DEP_FILE) as tmp:
+        pass
+except FileNotFoundError:
+    message = f"missing dep file: {DEP_FILE}"
+    print(message)
+    quit()
+
+BIB_FILE = DEP_FILE_BASE + ".bbl"
+try:
+    with open(BIB_FILE) as tmp:
+        pass
+except FileNotFoundError:
+    mssage = "missing bbl file: {BIB_FILE}"
+    print(message)
+    quit()
+
+
+def get_image_files():
+    img_files = []
+    with open(DEP_FILE, 'r') as f:
+        for line in f:
+            print(line)
+            if '*{file}' not in line:
+                continue
+            value = line.split('{')[2].split('}')
+            source = value[0]
+            _, e = os.path.splitext(source)
+            e = e.lower()[1:]
+            if e not in EXTENSIONS:
+                continue
+            print(source)
+            img_files.append(source)
+    return img_files
+
+
+def main():
+    """main"""
+    image_files = get_image_files()
+    file_list = []
+    manifest = ["# File list"]
+    MAIN_PDF = DEP_FILE_BASE + ".pdf"
+    file_list.append(MAIN_PDF)
+    manifest.append(f'- {MAIN_PDF}:\t compiled master pdf')
+    file_list.append(MAIN_FILE)
+    manifest.append(f'- {MAIN_FILE}:\t master tex file')
+    file_list.append(BIB_FILE)
+    manifest.append(f'- {BIB_FILE}:\t bib entries file')
+    for i, file_name in enumerate(image_files):
+        manifest.append(f'- {file_name}:\t Figure {i+1}')
+        file_list.append(file_name)
+    print("\n".join(manifest))
+    print(file_list)
+
+    # create manifest directory (if it doesnt exist)
+    if not os.path.exists(TARGET_DIR):
+        os.makedirs(TARGET_DIR)
+
+    # copy files over to manifest directory
+    for file_name in file_list:
+        dest = os.path.join(TARGET_DIR, file_name)
+        shutil.copyfile(file_name, dest)
+
+    # write manifest list as README.md
+    with open(os.path.join(TARGET_DIR, 'README.md'), 'w') as readme:
+        readme.write("\n".join(manifest))
+
+    # create archive
+    shutil.make_archive(DEP_FILE_BASE, 'zip', TARGET_DIR)
+
+
+if __name__ == '__main__':
+    main()
